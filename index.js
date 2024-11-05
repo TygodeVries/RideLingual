c1984c2b9d0a57983158dbe7163b943d8f67a9bd
Tygo <zttdevries@gmail.com>
2024-11-05 20:10:32 +0100

    Started on park list

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ function GetParks() {
         fetch("Translations/parks.json")
             .then((response) => response.json())
             .then((json) => console.log(json));
+
+            ShowParks(json["parks"])
     } catch (error) {
         console.error("Could not get parks!\n" + error);
         window.location.href = "/error";
+    }
+}
+
+function ShowParks(let parks) {
+    for (var i = 0; i < parks.lenght; i++) {
+        let park = parks[i];
+
+        document.append("Park: " + park.name);
     }
 }
